38454207fcca651968f0f226c5a825f23e91bce8
komfalcon <2.34061453e+08+komfalcon@users.noreply.github.com>
2026-09-06 12:55:23 +0000

    chore: refresh PWA shell cache

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aurikrex-bytes-v1';
+const CACHE = 'aurikrex-bytes-v2';
 const SHELL = ['/', '/manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
